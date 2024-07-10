fb3d3dec0184b9d4815c135daa27568805efe7ac
Matvey Shapkov <matveyka1504@mail.ru>
2024-07-10 20:18:05 +0500

    cookie-clicker edited

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,4 +2,10 @@ const cookieImage = document.getElementById('cookie');
 cookieImage.onclick = function() {
     const clicker = document.getElementById('clicker__counter');
     clicker.textContent = Number(clicker.textContent) + 1;
+    
+    if (cookie.width === 200) {
+        cookie.style.width = "220px";
+      } else {
+        cookie.style.width = "200px";
+      }
 }
